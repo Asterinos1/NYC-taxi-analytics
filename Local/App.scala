8f0d1a34287df04b3191db936b906953a3e17ba4
Asterinos Karalis <79656046+Asterinos1@users.noreply.github.com>
2025-07-23 17:12:25 +0300

    Added reservoirSampling method.
    Added method 'reservoirSampling' that is an implementation of
    Algorithm R. Current sampling size is 100k. As of now the code runs query1 first on all data, second on the sample size. Results are good so far.

--- Local/App.scala
+++ Local/App.scala
@@ -1,9 +1,10 @@
 import org.apache.spark.sql.SparkSession
-import org.apache.spark.sql.expressions.Window
 import org.apache.spark.sql.functions._
 import org.apache.spark.sql.types._
-import scala.util.Random
 
+import scala.jdk.CollectionConverters.IteratorHasAsScala
+import scala.util.Random
+import scala.reflect.ClassTag
 
 object Main extends App {
 
@@ -17,25 +18,25 @@ object Main extends App {
 
   //defining the schema to properly access the data in the csv file.
   val schema = StructType(Array(
-    StructField("VendorID", StringType, true),
-    StructField("tpep_pickup_datetime", StringType, true),
-    StructField("tpep_dropoff_datetime", StringType, true),
-    StructField("passenger_count", DoubleType, true),
-    StructField("trip_distance", DoubleType, true),
-    StructField("RatecodeID", StringType, true),
-    StructField("store_and_fwd_flag", StringType, true),
-    StructField("PULocationID", StringType, true),
-    StructField("DOLocationID", StringType, true),
-    StructField("payment_type", StringType, true),
-    StructField("fare_amount", DoubleType, true),
-    StructField("extra", DoubleType, true),
-    StructField("mta_tax", DoubleType, true),
-    StructField("tip_amount", DoubleType, true),
-    StructField("tolls_amount", DoubleType, true),
-    StructField("improvement_surcharge", DoubleType, true),
-    StructField("total_amount", DoubleType, true),
-    StructField("congestion_surcharge", DoubleType, true),
-    StructField("airport_fee", DoubleType, true)
+    StructField("VendorID", StringType, nullable = true),
+    StructField("tpep_pickup_datetime", StringType, nullable = true),
+    StructField("tpep_dropoff_datetime", StringType, nullable = true),
+    StructField("passenger_count", DoubleType, nullable = true),
+    StructField("trip_distance", DoubleType, nullable = true),
+    StructField("RatecodeID", StringType, nullable = true),
+    StructField("store_and_fwd_flag", StringType, nullable = true),
+    StructField("PULocationID", StringType, nullable = true),
+    StructField("DOLocationID", StringType, nullable = true),
+    StructField("payment_type", StringType, nullable = true),
+    StructField("fare_amount", DoubleType, nullable = true),
+    StructField("extra", DoubleType, nullable = true),
+    StructField("mta_tax", DoubleType, nullable = true),
+    StructField("tip_amount", DoubleType, nullable = true),
+    StructField("tolls_amount", DoubleType, nullable = true),
+    StructField("improvement_surcharge", DoubleType, nullable = true),
+    StructField("total_amount", DoubleType, nullable = true),
+    StructField("congestion_surcharge", DoubleType, nullable = true),
+    StructField("airport_fee", DoubleType, nullable = true)
   ))
 
   private val taxiDF = spark.read
@@ -46,15 +47,46 @@ object Main extends App {
     .withColumn("dropoff_datetime", to_timestamp(col("tpep_dropoff_datetime")))
     .withColumn("hour", hour(col("pickup_datetime")))
     .filter(col("passenger_count") > 0)  //filtering some invalid cases.
-//    .filter(col("tip_amount") >= 0)
+  //    .filter(col("tip_amount") >= 0)
 
   println(s"Total rows: ${taxiDF.count()}")
 
   //taxiDF.limit(10).show(truncate = false)
 
+  //we're going to use Algorithm R to get our sample.
+  private def reservoirSample[T: ClassTag](input: Iterator[T], k: Int): Array[T] = {
+    val reservoir = new Array[T](k)
+    val random = new Random()
+
+    for ((elem, i) <- input.zipWithIndex) {
+      if (i < k) {
+        reservoir(i) = elem
+      } else {
+        val j = random.nextInt(i + 1)
+        if (j < k) {
+          reservoir(j) = elem
+        }
+      }
+    }
+
+    reservoir
+  }
+
+  //getting the sample from the algorithm.
+  private val sampleSize = 100_000  // initial value, perhaps it can be changed later.
+  private val iter = taxiDF.toLocalIterator().asScala //before we call our samplingMethod we gather all data on the driver node
+  private val sampledRows = reservoirSample(iter, sampleSize) //we then do the sampling
+  private val sampleRDD = spark.sparkContext.parallelize(sampledRows.toList) //then redistribute the data again.
+  private val sampleDF = spark.createDataFrame(sampleRDD, taxiDF.schema) //our sample dataframe is ready
+
+
+  println(s"Sample size: ${sampleDF.count()} rows")
+
   //Query1: Peak Hour Tipping Behavior – Compute average tips per passenger by hour.
 
+  //No sample
   val start = System.nanoTime()
+
   private val query1DF = taxiDF
     .filter(col("passenger_count") > 0)
     .filter(col("tip_amount") >= 0)
@@ -65,13 +97,31 @@ object Main extends App {
       count("*").alias("trip_count")
     )
     .orderBy("hour")
+
   val end = System.nanoTime()
+
   private val durationSeconds = (end - start) / 1e9d
   // Show the results
   query1DF.show()
   println(f"Query 1 executed in $durationSeconds%.2f seconds")
 
+  //With Sample
+  private val startSample = System.nanoTime()
+  private val query1SampleDF = sampleDF
+    .filter(col("tip_amount") >= 0)
+    .withColumn("tip_per_passenger", col("tip_amount") / col("passenger_count"))
+    .groupBy("hour")
+    .agg(
+      round(avg("tip_per_passenger"), 2).alias("avg_tip_per_passenger"),
+      count("*").alias("trip_count")
+    )
+    .orderBy("hour")
+  private val endSample = System.nanoTime()
+  private val timeSample = (endSample - startSample) / 1e9
 
+  query1SampleDF.show()
+  println(f"Query 1 (Sample) executed in $timeSample%.2f seconds")
 
   spark.stop()
+  
 }
